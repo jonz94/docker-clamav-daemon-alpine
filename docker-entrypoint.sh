e892062746884ef0dc2e6bdf80b658751d570c72
jonz94 <jody16888@gmail.com>
2019-09-22 03:41:04 +0800

    ✅ fix: make sure container will keep running

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 set -e
 
-# init
-if [ ! -f /var/lib/clamav/main.cvd ]; then
-  freshclam
-fi
-
 # start fresh daemon, check for update 6 times per day
 freshclam -d -c 6
 
 # start clamav daemon
-clamd
+clamd &
+
+# recognize PIDs
+pidlist=$(jobs -p)
+
+# initialize latest result var
+latest_exit=0
+
+# define shutdown helper
+function shutdown() {
+  trap "" SIGINT
+
+  for single in $pidlist; do
+    if ! kill -0 $single 2>/dev/null; then
+      wait $single
+      latest_exit=$?
+    fi
+  done
+
+  kill $pidlist 2>/dev/null
+}
+
+# run shutdown
+trap shutdown SIGINT
+wait -n
+
+# return received result
+exit $latest_exit
